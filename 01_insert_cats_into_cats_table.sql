52b5be92f58df171b64d87aea4691c79a2ec7851
Caroline Shields <caroline.shields@amkor.com>
2017-11-30 19:46:58 +0000

    initial SQL codealongs

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,6 @@
+--INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, 'American Shorthair');
+--INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Hana", 1, "Tabby", 21800);
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Grumpy Cat", 4, "Persian", 181600);
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Lil' Bub", 2, "Tortoiseshell", 2000000);
